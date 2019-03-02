75e9e75247eedbe6bb23979be1511768808681d9
Alberto Bezerra <albertofbezerra@gmail.com>
2019-03-02 01:50:36 -0300

    Alberto Bezerra
    Atualizações do IDE

--- app/src/main/java/equipebezerradasilva/guardacorpo/splash.java
+++ app/src/main/java/equipebezerradasilva/guardacorpo/splash.java
@@ -1,13 +1,20 @@
 package equipebezerradasilva.guardacorpo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.onesignal.OneSignal;
 
 public class splash extends AppCompatActivity implements Runnable {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,8 @@ public class splash extends AppCompatActivity implements Runnable {
         Handler handler = new Handler();
         handler.postDelayed(this, 2000);
 
+
+
     }
 
     @Override
@@ -29,4 +38,6 @@ public class splash extends AppCompatActivity implements Runnable {
         startActivity(new Intent(this, estacionario.class));
         finish();
     }
+
+
 }
