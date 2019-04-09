b00442a62f8e7743798ac17860d7951c1928391d
Alberto Bezerra <albertofbezerra@gmail.com>
2019-04-08 22:51:48 -0300

    PPP
    Fase inicial

--- app/src/main/java/equipebezerradasilva/guardacorpo/estacionario.java
+++ app/src/main/java/equipebezerradasilva/guardacorpo/estacionario.java
@@ -43,7 +43,7 @@ public class estacionario extends AppCompatActivity implements Runnable {
                 R.drawable.menu_principal_dds, R.drawable.menu_principal_sinalizacao, R.drawable.menu_principal_mapa, R.drawable.menu_principal_datas, R.drawable.menu_principal_tecnico,
                 R.drawable.menu_principal_penalidades, R.drawable.menu_principal_aso, R.drawable.menu_principal_cid, R.drawable.menu_principal_cipa, R.drawable.menu_principal_historia,
                 R.drawable.menu_principal_nbr, R.drawable.menu_principal_nho, R.drawable.menu_principal_epi, R.drawable.menu_principal_cnae, R.drawable.menu_principal_cnpj,
-                R.drawable.menu_principal_ordemdeservico,
+                R.drawable.menu_principal_ordemdeservico, R.drawable.menu_principal_ppp,
 
         };
 
@@ -180,6 +180,12 @@ public class estacionario extends AppCompatActivity implements Runnable {
                 if (position == 20) {
                     Intent myIntent = new Intent(view.getContext(), ordemdeservico.class);
                     startActivityForResult(myIntent, 20);
+                    displayInterstitial();
+                }
+
+                if (position == 21) {
+                    Intent myIntent = new Intent(view.getContext(), ppp.class);
+                    startActivityForResult(myIntent, 21);
                     displayInterstitial();
                 }
 
