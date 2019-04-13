4c8f13e810ded865d26e262a07e2a5185b1a5bc6
Alberto Bezerra <albertofbezerra@gmail.com>
2019-04-13 01:14:36 -0300

    PPP

--- app/src/main/java/equipebezerradasilva/guardacorpo/ppp.java
+++ app/src/main/java/equipebezerradasilva/guardacorpo/ppp.java
@@ -1,7 +1,16 @@
 package equipebezerradasilva.guardacorpo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class ppp extends AppCompatActivity {
 
@@ -9,5 +18,22 @@ public class ppp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ppp);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView_ppp);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+    }
+
+    public void voltar_estacionario(View view) {
+
+        Intent secondActivity = new Intent(this, estacionario.class);
+        startActivity(secondActivity);
+    }
+
+    public void exemploppp(View view) {
+
+        Intent browserInternt = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.inss.gov.br/wp-content/uploads/2017/05/IN85PRESINSSanexoI_PPP.pdf"));
+        startActivity(browserInternt);
     }
 }
