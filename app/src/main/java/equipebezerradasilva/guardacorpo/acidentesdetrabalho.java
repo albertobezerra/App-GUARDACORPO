1a759d0e367e7b8bd65c875d5a81b9565260edb5
Alberto Bezerra <albertofbezerra@gmail.com>
2019-05-03 23:54:24 -0300

    Modulo: Acidentes de Trabalho

---
+++ app/src/main/java/equipebezerradasilva/guardacorpo/acidentesdetrabalho.java
@@ -0,0 +1,28 @@
+package equipebezerradasilva.guardacorpo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class acidentesdetrabalho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acidentesdetrabalho);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView_acidentedetrabalho);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    public void voltar_estacionario(View view) {
+
+        Intent secondActivity = new Intent(this, estacionario.class);
+        startActivity(secondActivity);
+    }
+}
